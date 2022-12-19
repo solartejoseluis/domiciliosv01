11e4808ce7bc8ab4f45d32094ef5e597c34d3a4b
solartejoseluis <solartejoseluis@gmail.com>
2022-12-19 18:09:18 -0500

    prueba de select2 basico
     funcionó de manera basica

--- scripts.js
+++ scripts.js
@@ -56,29 +56,58 @@
 //   }
 // });
 
-$('#slct-barrio').select2({
-    ajax: {
-        url: "getBarrio.php",
-        type:'GET',
-        dataType: 'json',
-        delay: 250,
-        data: function (params) {
-            return {
-                q: params.term // search term
-            };
-        },
-        processResults: function (data) {
-            // parse the results into the format expected by Select2.
-            // since we are using custom formatting functions we do not need to
-            // alter the remote JSON data
-            return {
-                results: data
-            };
-        },
-        cache: true
-    },
-    minimumInputLength: 2
+// $('#slct-barrio').select2({
+//     ajax: {
+//         url: "getBarrio.php",
+//         type:'GET',
+//         dataType: 'json',
+//         delay: 250,
+//         data: function (params) {
+//             return {
+//                 q: params.term // search term
+//             };
+//         },
+//         processResults: function (data) {
+//             // parse the results into the format expected by Select2.
+//             // since we are using custom formatting functions we do not need to
+//             // alter the remote JSON data
+//             return {
+//                 results: data
+//             };
+//         },
+//         cache: true
+//     },
+//     minimumInputLength: 2
+// });
+
+
+$(document).ready(function() {
+  $('#slct-barrio').select2();
 });
+
+// $('#slct-barrio').select2({
+//     ajax: {
+//         url: "getBarrio.php",
+//         type:'GET',
+//         dataType: 'json',
+//         delay: 250,
+//         data: function (params) {
+//             return {
+//                 q: params.term // search term
+//             };
+//         },
+//         processResults: function (data) {
+//             // parse the results into the format expected by Select2.
+//             // since we are using custom formatting functions we do not need to
+//             // alter the remote JSON data
+//             return {
+//                 results: data
+//             };
+//         },
+//         cache: true
+//     },
+//     minimumInputLength: 2
+// });
 
 
 
